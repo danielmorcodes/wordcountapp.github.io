0726606c6935dedcfde641faef78a8a600c288cd
Daniel Mor <danielfalonipe@gmail.com>
2019-03-07 17:58:18 -0500

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+# wordcountapp.github.io
+A simple word count app created with HTML/CSS and JavaScript
+
+It allows you to find the number of characters in the input word(s)
---
+++ custom.js
@@ -0,0 +1,27 @@
+
+// Function that Finds the longest word in a string
+function findLongestWordLength(str) {
+  var words = str.split(" ");
+  var maxLength = 0;
+
+  for(let i = 0; i < words.length; i++){
+    if (words[i].length > maxLength){
+      maxLength = words[i].length;
+    }
+  }
+  return "Longest word = " + maxLength + " characters.";
+}
+
+function resetButton() { 
+  document.getElementById("text").value = "";  // resets the values in the input/output boxes to empty
+  document.getElementById("output").value = "";
+
+}
+
+function findLenButton() {
+  const val = document.getElementById("text").value; // get the value from your text box here
+  
+  document.getElementById("output").value = findLongestWordLength(val); // displays the result in the output box
+
+}
+
